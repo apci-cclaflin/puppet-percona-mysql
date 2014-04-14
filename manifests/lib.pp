1f8a80ad63b7b1f522355ff617a59a72cd4f9402
Cory Claflin <cclaflin@allplayers.com>
2014-04-14 14:30:09 -0500

    [#69482734] make privileges a passed dynamic variable to support read-only api user

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -2,8 +2,8 @@ class mysqllib {
 
   include mysql::client
 
-  define mysqldb( $user, $password, $db = $name, $clienthost = 'localhost', $host = 'localhost', $privileges = 'ALL' ) {
-    exec { "create-${db}-db-${clienthost}":
+  define mysqldb( $user, $password, $db, $clienthost, $host = 'localhost', $privileges ) {
+    exec { "create-${db}-db-${user}-${clienthost}":
       unless => "/usr/bin/mysql -h${host} -uroot -e \"SHOW GRANTS FOR '${user}'@'${clienthost}';\" | grep 'ON `${db}`.*'",
       command => "/usr/bin/mysql -h${host} -uroot -e \"CREATE DATABASE IF NOT EXISTS ${db}; GRANT ${privileges} ON ${db}.* TO '${user}'@'${clienthost}' IDENTIFIED BY '${password}'; FLUSH PRIVILEGES;\"",
       require => Service["mysql"],
